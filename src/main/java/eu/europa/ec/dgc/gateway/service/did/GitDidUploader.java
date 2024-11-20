22d8f8ee0e01e6223795b913e2154d36a627a2f1
Shrey Bansod <shrey.bansod@t-systems.com>
2024-11-20 12:53:03 +0530

    Updated code as per review comments

--- src/main/java/eu/europa/ec/dgc/gateway/service/did/GitDidUploader.java
+++ src/main/java/eu/europa/ec/dgc/gateway/service/did/GitDidUploader.java
@@ -20,24 +20,23 @@
 
 package eu.europa.ec.dgc.gateway.service.did;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import eu.europa.ec.dgc.gateway.config.DgcConfigProperties;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import java.time.Instant;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Service;
+
+import eu.europa.ec.dgc.gateway.config.DgcConfigProperties;
+import lombok.extern.slf4j.Slf4j;
 
 @ConditionalOnExpression("'${dgc.did.didUploadProvider}'.contains('git')")
 @Service
@@ -50,63 +49,72 @@ public class GitDidUploader implements DidUploader {
         this.configProperties = configProperties;        
     }
     
-   
-    private Request prepareRequest(String owner, String repo, String path, String content, String token)
-            throws JsonProcessingException {
-        String url = "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + path;
+    
+    @Override
+    public void uploadDid(byte[] content) {
+    	Path targetDirectory = Paths.get(configProperties.getDid().getGit().getWorkdir() 
+    	          + File.separator 
+    	          + configProperties.getDid().getGit().getPrefix());
+    	
+    	deleteDirectoryAndContents(configProperties.getDid().getGit().getWorkdir());
+    	
+    	try {
+            Git.cloneRepository()
+            .setURI(configProperties.getDid().getGit().getUrl())
+            .setDirectory(new File(configProperties.getDid().getGit().getWorkdir()))
+            .setCredentialsProvider(
+                new UsernamePasswordCredentialsProvider(
+                "anonymous", configProperties.getDid().getGit().getPat()))
+                .call();
+        } catch (Exception e) {
+            log.error("Failed to clone repository {}: {}",
+                      configProperties.getDid().getGit().getUrl(), e.getMessage());
+        }
+    	
+    	try {
+    		File outputFile = new File(targetDirectory.toString() + File.separator + "did.json");
+    		try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+    		    outputStream.write(content);
+    		}
+            Git git = Git.open(new File(configProperties.getDid().getGit().getWorkdir()));
+            git.add().addFilepattern(configProperties.getDid().getGit().getPrefix() + File.separator + outputFile.getName()).call();
+            git.commit().setMessage("Added DID files on " + Instant.now()).call();
+            git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(
+                "anonymous", configProperties.getDid().getGit().getPat())).call();
+            git.close();
+            log.info("Successfully uploaded DID files to Git repository {}",
+                     configProperties.getDid().getGit().getUrl());
+        } catch (GitAPIException | IOException e) {
+            log.error("Error during Git commit & push: {}",e.getMessage());
+        }
+    	
+    }
 
-        // Encode file content to Base64
-        String encodedContent = Base64.getEncoder().encodeToString(content.getBytes(StandardCharsets.UTF_8));
-
-        // Prepare JSON payload
-        Map<String, String> jsonMap = new HashMap<>();
-        jsonMap.put("message", "Automated commit message");
-        jsonMap.put("content", encodedContent);
-        jsonMap.put("branch", configProperties.getDid().getGit().getBranch());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonPayload = objectMapper.writeValueAsString(jsonMap);
-
-        RequestBody body = RequestBody.create(jsonPayload, MediaType.parse("application/json"));
-        Request request = new Request.Builder()
-                .url(url)
-                .header("Authorization", "Bearer " + token)
-                .header("Accept", "application/vnd.github+json")
-                .header("X-GitHub-Api-Version", "2022-11-28")
-                .put(body)
-                .build();
-        return request;
+    private void deleteDirectoryAndContents(String directoryPath) {
+        Path dir = Paths.get(directoryPath);
+        if (dir.toFile().exists()) {
+    
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+                for (Path path : stream) {
+                    if (Files.isDirectory(path)) {
+                        deleteDirectoryAndContents(path.toString());
+                    } else {
+                        Files.delete(path);
+                    }
+                }
+            } catch (IOException e) {
+                log.error("Error deleting file {}",e.getMessage());
+            }
+            try {
+                Files.delete(dir);
+            } catch (IOException e) {
+                log.error("Error deleting root directory {}",e.getMessage());
+            }
+        } else {
+            log.info("Directory {} does not exist, skippig deletion", dir);
+        }
     }
     
-    private void uploadFileToGitHub(String owner, String repo, String path, String content, String token) 
-        throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = prepareRequest(owner, repo, path, content, token);
-
-        Response response = client.newCall(request).execute();
-        if (response.isSuccessful()) {
-            log.info("File uploaded successfully");
-        } else {
-            log.error("Failed to upload file: " + response.message());
-        }
-    }
-
-    @Override
-    public void uploadDid(byte[] content) {        
-        String fileContent = new String(content, StandardCharsets.UTF_8);
-        try {
-            uploadFileToGitHub(configProperties.getDid().getGit().getOwner(), 
-                    configProperties.getDid().getGit().getWorkdir(), 
-                    configProperties.getDid().getGit().getPrefix() + "/" + configProperties.getDid().getGit().getUrl(), 
-                    fileContent, 
-                    configProperties.getDid().getGit().getPat());
-        } catch (IOException e) {
-            log.error("Error occured while uploading a file to Github");
-        }
-        log.info("Upload successful");
-    }    
-
     @Override
     public void uploadDid(String subContainer, byte[] content) {
               
